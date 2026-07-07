a1ddf8c7b4921990784c8b1350f0f945700ec018
vamos16 <kartasgoksu16@gmail.com>
2026-07-07 13:07:11 +0300

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// Supabase bağlantı ayarları
-// GitHub Pages için hazırdır.
-
-export const SUPABASE_URL = "https://cvpbhxqusmzklktentcz.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_m0v7AN24IauzyeS5IHFd1g_JcRxEF7w";
